b760cd4ed58c9e3061ac23101a7dd8abafc2e0e6
Dugiii <dugidemeg@gmail.com>
2025-12-17 09:23:47 -0600

    adding todo items

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,56 @@
+import { useState } from 'react'
 import './styles.css'
 
 
 function App() {
+  const [newItem, setNewItem] = useState('');
+  const [newTodos, setNewTodos] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setNewTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        {
+          id:crypto.randomUUID(),
+          title: newItem,
+          completed: false
+        }
+      ]
+    })
+  }
 
   return (
-    <form className='new-item-form'>
-      <h1>Todo App</h1>
-      <div className='form-row'>
-        <label htmlFor='item'>New Item</label>
-        <input type='text' id='item'/>
-
-      </div>
-    </form>
+    <>
+      <form onSubmit={handleSubmit} className='new-item-form'>
+        <h1>Todo App</h1>
+        <div className='form-row'>
+          <label htmlFor='item'>New Item</label>
+          <input 
+            type='text' 
+            value={newItem} 
+            id='item'
+            onChange={e => setNewItem(e.target.value)}
+          />
+        </div>
+        <button className='btn'>Add</button>
+      </form>
+      <h2 className='header'>Todo List</h2>
+      <ul className='list'>
+        {newTodos.map(newTodo => {
+          return (
+            <li>
+              <label>
+                <input type='checkbox' checked={newTodo.completed} />
+                {newTodo.title}
+              </label>
+              <button className='btn btn-danger'>Delete</button>
+            </li>
+          )
+        })}
+      </ul>
+    </>
   )
 }
 
